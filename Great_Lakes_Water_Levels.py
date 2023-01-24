4f55819095300d795675fa285fcb6e181a6bfc23
Tyler Matson <matsont1@msu.edu>
2023-01-24 16:17:23 -0500

    added the lake superior code

--- Great_Lakes_Water_Levels.py
+++ Great_Lakes_Water_Levels.py
@@ -1,19 +1,9 @@
-""""
-To use this notebook for your in-class assignment, you will need these 
-files, which you shoujld have downloaded:
-* mhu.csv -- Lake Michigan and Lake Huron
-* sup.csv -- Lake Superior
-* eri.csv -- Lake Erie
-* ont.csv -- Lake Ontario
+# +
 
-As instructed in the in-class activity notebook for today, you are 
-only expected to complete one PART below. Do not worry if your group 
-is not big enough to finish all parts below, but if you have extra 
-time, you're welcome to do so.
-""""
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+# -
 
 
 # PART 1
@@ -26,7 +16,12 @@ import matplotlib.pyplot as plt
 # Using the Superior Dataset, plot the Water Level, the second column, 
 # as a function of time years
 
-
+sup = pd.read_csv("sup.csv")
+year = sup["year"]
+lake_levels = sup["lake levels"]
+plt.plot(year, lake_levels, label= "Water level")
+plt.title("Lake Levels Over Time")
+plt.legend()
 
 # PART 3
 # Using the Erie Dataset, plot the Water Level, the second column, 
@@ -55,7 +50,7 @@ import matplotlib.pyplot as plt
 
 
 # PART 7
-#Using the Superior and Ontario Datasets, plot the Superior Water 
+# Using the Superior and Ontario Datasets, plot the Superior Water 
 # Level vs Ontario Water Level to see if there is any correlation 
 # between the water levels.
 
